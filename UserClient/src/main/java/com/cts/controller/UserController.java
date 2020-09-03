332baf97a7716d2e5e627cc46658b90a8da31075
Poojajmpatil <41111770+Poojajmpatil@users.noreply.github.com>
2020-09-03 22:42:58 +0530

    Add files via upload

---
+++ UserClient/src/main/java/com/cts/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.cts.controller;
+import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.cts.model.User;
+//import com.cts.service.IUserService;
+import com.cts.service.UserService;
+
+@RestController
+@Validated
+public class UserController {
+ 
+	@Autowired
+	UserService userService;
+//	
+	@GetMapping("/users")
+	public List<User> getUser(){
+		return userService.getAllUser();
+	}
+//	
+//	@GetMapping("/users")
+//	public List<String> getUser(){
+//	return Stream.of("abc","bcd").collect(Collectors.toList());
+//	}	
+	
+	
+	
+	@PostMapping("/user")
+	public void insertStudent(@Valid @RequestBody User user) {
+		userService.saveUser(user);
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.PUT)
+	public HttpStatus updateUser(@RequestBody User user) {
+		return userService.updateUser(user)  ? HttpStatus.ACCEPTED : HttpStatus.BAD_REQUEST;
+	}
+	
+	
+}
